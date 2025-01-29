a9b9b0c6aaa05fc286be9b846408a8752043f42a
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2025-01-29 17:52:16 +0800

    Add Zod and Bcrypt modules and specify rules of Key on memberSchema

--- models/member.js
+++ models/member.js
@@ -1,10 +1,29 @@
 import mongoose from 'mongoose';
 
 const memberSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    role: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 25,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    role: {
+        type: String,
+        default: 'member',
+        enum: ['admin', 'member'],
+    },
 })
 
 const Member = mongoose.model('Member', memberSchema);
